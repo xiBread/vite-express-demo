512ae2bfab2109287478f6206f65b5c8033a04f9
xiBread <oscar.leevermeren@gmail.com>
2025-03-08 02:25:07 -0500

    feat: add neon

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { neon } from "@neondatabase/serverless";
 
 const app = express();
+const sql = neon(process.env.DATABASE_URL);
 
-app.get("/hello", (req, res) => {
-	res.send("Hello Vite!");
+app.get("/hello", async (req, res) => {
+	const [row] = await sql`SELECT 'Hello, world!' as message`;
+
+	res.send(row.message);
 });
 
 ViteExpress.listen(app, 3000, () =>

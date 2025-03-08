c1a1ba21a91a3403875a0e381a2f92561c2ba912
xiBread <oscar.leevermeren@gmail.com>
2025-03-08 01:27:50 -0500

    feat: integrate neon

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,16 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { neon } from "@neondatabase/serverless";
 
 const app = express();
+const sql = neon(process.env.DATABASE_URL);
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite!");
+app.get("/hello", async (req, res) => {
+	const [row] = await sql`SELECT 'Hello!' AS greeting`;
+
+	res.send(row.greeting);
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+	console.log("Server is listening on port 3000..."),
 );

c8886773205b1012a1ced403025e478b58496f81
xiBread <oscar.leevermeren@gmail.com>
2025-03-08 23:40:59 -0500

    fix: specify entry points

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+import path from "node:path";
+import url from "node:url";
 import { defineConfig } from "vite";
+
+const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	build: {
 		target: "esnext",
+		rollupOptions: {
+			input: {
+				main: path.resolve(dirname, "index.html"),
+				newRecipe: path.resolve(dirname, "recipes/new/index.html"),
+			},
+		},
 	},
 });
